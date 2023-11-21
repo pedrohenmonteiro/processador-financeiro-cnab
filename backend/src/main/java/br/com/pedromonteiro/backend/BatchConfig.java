9b04b6428a8fe3c349bcc740e62cfcb7c67fc182
Pedro Monteiro <pedromont436@gmail.com>
2023-11-21 00:05:22 -0300

    configurando Reader

--- backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
+++ backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
@@ -9,8 +9,12 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -34,12 +38,29 @@ public class BatchConfig {
     }
     
     @Bean
-    Step step(ItemReader<TransacaoCNAB> reader, ItemProcessor processor, ItemWriter<Transacao> writer) {
+    Step step(ItemReader<TransacaoCNAB> reader, ItemProcessor<TransacaoCNAB, Transacao> processor, ItemWriter<Transacao> writer) {
         return new StepBuilder("step", jobRepository)
-        .chunk(1000, transactionManager)
+        .<TransacaoCNAB,Transacao>chunk(1000, transactionManager)
         .reader(reader)
         .processor(processor)
         .writer(writer)
+        .build();
+    }
+
+    @Bean
+    FlatFileItemReader<TransacaoCNAB> reader() {
+        return new FlatFileItemReaderBuilder<TransacaoCNAB>()
+        .name("reader")
+        .resource(new FileSystemResource("files/CNAB.txt"))
+        .fixedLength()
+        .columns(
+            new Range(1,1), new Range(2, 9),
+            new Range(1,1), new Range(2, 9),
+            new Range(10,19), new Range(43, 48),
+            new Range(1,1), new Range(63, 80)
+        )
+        .names("tipo", "data", "valor", "cpf", "cartao", "hora", "donoDaLoja", "nomeDaLoja")
+        .targetType(TransacaoCNAB.class)
         .build();
     }
 }   
