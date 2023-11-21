651e94a63d8e17a04ed7c5d977d7b837af503c4b
Pedro Monteiro <pedromont436@gmail.com>
2023-11-20 23:47:24 -0300

    criando TransacaoCNAB

--- backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
+++ backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
@@ -34,7 +34,7 @@ public class BatchConfig {
     }
     
     @Bean
-    Step step(ItemReader reader, ItemProcessor processor, ItemWriter writer) {
+    Step step(ItemReader<TransacaoCNAB> reader, ItemProcessor processor, ItemWriter writer) {
         return new StepBuilder("step", jobRepository)
         .chunk(1000, transactionManager)
         .reader(reader)
