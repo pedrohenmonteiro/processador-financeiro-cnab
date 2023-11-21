d703c21ed0a5834672180b577e6596494d8c799d
Pedro Monteiro <pedromont436@gmail.com>
2023-11-20 23:39:52 -0300

    criando job e step

--- backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
+++ backend/src/main/java/br/com/pedromonteiro/backend/BatchConfig.java
@@ -1,5 +1,15 @@
 package br.com.pedromonteiro.backend;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -7,10 +17,29 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class BatchConfig {
     
     private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
 
-    public BatchConfig(PlatformTransactionManager transactionManager) {
+    public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
         this.transactionManager = transactionManager;
+        this.jobRepository = jobRepository;
     }
 
+    @Bean
+    Job job(Step step) {
+
+        return new JobBuilder("job", jobRepository)
+        .start(step)
+        .incrementer(new RunIdIncrementer())
+        .build();
+    }
     
-}
+    @Bean
+    Step step(ItemReader reader, ItemProcessor processor, ItemWriter writer) {
+        return new StepBuilder("step", jobRepository)
+        .chunk(1000, transactionManager)
+        .reader(reader)
+        .processor(processor)
+        .writer(writer)
+        .build();
+    }
+}   
